93785ebf1d6dcf28be9cde25c33ae3d63fbd1833
avk3 <avk_3@mail.ru>
2017-02-24 22:44:07 +0200

    Deepend Ransomware Payment Sites Feed
    Python script to download Deepend's feed into a .CSV file every 24
    hours.

---
+++ deeperend.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import requests
+import sched, time
+
+s = sched.scheduler(time.time, time.sleep)
+
+def main(sc): 
+    print "Downloading Ransomware feed..."
+    ioc = []
+    feed_file = requests.get('https://files.deependresearch.org/feeds/ransomware/ransomware-payment-sites.txt', verify=False).content
+    outfile = 'domain,notes\n'
+    for line in feed_file.splitlines():
+        if line.startswith('#') or '.' not in line:
+            continue
+        outfile += '%s,DeepEndResearch Suspected Ransomware Payment Site\n' % line
+    with open('ransomware_payment_site.csv', 'w') as fh:
+        fh.write(outfile)
+    s.enter(86400, 1, main, (sc,)) 
+    s.run()
+     
+main(0)
